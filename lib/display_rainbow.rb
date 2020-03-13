a1c0ae178e73b38c06c88d378bf50bf896b3d098
Marcelo Bini <marcelobini@gmail.com>
2020-03-13 14:56:49 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,4 +2,4 @@
 def display_rainbow
   puts display_rainbow(['red','orange','yellow','green','blue','indigo','violet'])
 end
-display_rainbow
+display_rainbow = "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
